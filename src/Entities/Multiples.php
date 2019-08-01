39c3a4036a584ca4691d06c94c35aceb5b589eee
Gabriel Portes <gabriel-portes@hotmail.com>
2019-08-01 10:02:37 -0300

    Corrigido espaços em branco

--- src/Entities/Multiples.php
+++ src/Entities/Multiples.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Gabriel\Multiples\Entities;
 
 class Multiples
@@ -8,43 +9,42 @@ class Multiples
 		'IT',
 		'Linianos'
 	];
-	
-    /**
-     * Print all numbers from 1 to $to
-     * 
-     * @param int $to last number to print
-     */
+
+	/**
+	 * Print all numbers from 1 to $to
+	 * 
+	 * @param int $to last number to print
+	 */
 	public function printNumbers(int $to)
 	{
 		$from = 1;
-		foreach (range($from, $to) as $number) {			
+		foreach (range($from, $to) as $number) {
 			echo self::testMultiple($number), '<br>';
-		}        
+		}
 	}
-	
-    /**
-     * Test if a number is multiple of 3, 5 or both at same time
-     * 
-     * @param int $number number to test
-     * @return string
-     * - When $number is multiple of 3, return the word "Linio"
-     * - When $number is multiple of 5, return the word "IT" 
-     * - When $number is multiple of 3 and 5, at same time, return the word "Linianos"
-     * - Otherwise returns $number
-     */
+
+	/**
+	 * Test if a number is multiple of 3, 5 or both at same time
+	 * 
+	 * @param int $number number to test
+	 * @return string
+	 * - When $number is multiple of 3, return the word "Linio"
+	 * - When $number is multiple of 5, return the word "IT" 
+	 * - When $number is multiple of 3 and 5, at same time, return the word "Linianos"
+	 * - Otherwise returns $number
+	 */
 	protected static function testMultiple(int $number): string
 	{
 		$mod3 = intval(bcmod($number, 3));
 		$mod5 = intval(bcmod($number, 5));
 		$arrRemainders = [$mod3, $mod5, ($mod3 + $mod5)];
-		
-		for  ($i = 0; $i < count(self::OPTIONS); $i++) {
+
+		for ($i = 0; $i < count(self::OPTIONS); $i++) {
 			if ($arrRemainders[$i] === 0) {
 				return (string) self::OPTIONS[$i];
 			}
 		}
-		
+
 		return (string) $number;
 	}
 }
-?>
