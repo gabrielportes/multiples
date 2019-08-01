e356de9e644d4a780c19ac098bcf6deacbac4883
Gabriel Portes <35439823+gabrielportes@users.noreply.github.com>
2019-07-31 22:07:19 -0300

    Code refactoring entire class Multiples

--- src/Entities/Multiples.php
+++ src/Entities/Multiples.php
@@ -1,54 +1,51 @@
 <?php
-    namespace Gabriel\Multiples\Entities;
+namespace Gabriel\Multiples\Entities;
 
-    class Multiples{
-        private $count              = 1;
-        private $arrayTextOptions   = array("Linio", "IT", "Linianos");
-
-        // Print all the numbers from 1 to 100
-        public function printNumbers($size){
-            for($i=0; $i<$size; $i++){
-                $count = $this->getCount();
-        
-                echo $this->testMultiple($count), "<br>";
-                $this->setCount(++$count);
-            }        
-        }
-        
-        // Test if a number is multiple of 3, 5 or both at same time
-        // - For multiples of 3, instead of the number, print "Linio" 
-        // - For multiples of 5, instead of the number, print "IT" 
-        // - For numbers, which are multiples of both 3 and 5, print "Linianos" 
-        public function testMultiple($number){
-            $arrayTextOptions   = $this->getArrayTextOptions();
-            $mod3               = $this->calculatesRemainder($number, 3);
-            $mod5               = $this->calculatesRemainder($number, 5);
-            $arrayMod           = array($mod3, $mod5, $mod3+$mod5);
-            $print              = $number;
-
-            for($i=0; $i<count($arrayTextOptions); $i++){
-                if( $arrayMod[$i] === 0 )
-                    $print = $arrayTextOptions[$i]; 
-            }
-            
-            return $print;
-        }
-
-        // Caculates the remainder of a division
-        public function calculatesRemainder($dividend, $divisor){
-            return $dividend % $divisor;
-        }
-
-        public function getCount(){
-            return $this->count;
-        }
-
-        public function setCount($count){
-            $this->count = $count;
-        }
-
-        public function getArrayTextOptions(){
-            return $this->arrayTextOptions;
-        }
-    }
+class Multiples
+{
+	const OPTIONS = [
+		'Linio',
+		'IT',
+		'Linianos'
+	];
+	
+    /**
+     * print all numbers from 1 to $to
+     * 
+     * @param int $to last number to print
+     */
+	public function printNumbers(int $to)
+	{
+		$count = 1;
+		for ($i = 0; $i < $to; $i++) {			
+			echo self::testMultiple($count), '<br>';
+			$count++;
+		}        
+	}
+	
+    /**
+     * Test if a number is multiple of 3, 5 or both at same time
+     * 
+     * @param int $number number to test
+     * @return string 
+     * - When $number is multiple of 3, returns the word "Linio" 
+	 * - When $number is multiple of 5, returns the word "IT" 
+	 * - When $number, is multiple of 3 and 5, at same time, returns the word "Linianos"
+     * - Otherwise returns $number
+     */
+	protected static function testMultiple(int $number): string
+	{
+		$mod3 = intval(bcmod($number, 3));
+		$mod5 = intval(bcmod($number, 5));
+		$arrRemainders = [$mod3, $mod5, ($mod3 + $mod5)];
+		
+		for  ($i = 0; $i < count(self::OPTIONS); $i++) {
+			if ($arrRemainders[$i] === 0) {
+				return (string) self::OPTIONS[$i];
+			}
+		}
+		
+		return (string) $number;
+	}
+}
 ?>
